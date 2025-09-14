6a4fd074833ff0866e20a9b5f229e060e73d1871
FahadAliHashmi2011 <faiezali2006@gmail>
2025-09-14 16:24:27 +0100

    x

---
+++ index.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+if a and b and c :
+    print("all the numbers have a boolean value as true")
+else:
+    print("at least one number has a boolean value of false")
+
+
+a = 10
+b =-10
+c = 0
+if a > 0 or b > 0:
+    print("either number is greater than 0")
+else: 
+    print(" no number is greater than 0")
+
+if a > 0 or b > 0:
+    print("either number is greater than 0")
+else: 
+    print(" no number is greater than 0")
