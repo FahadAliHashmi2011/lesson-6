12fec5c2cc10f8b4dba1b67bf5242199e26a5a2e
FahadAliHashmi2011 <faiezali2006@gmail>
2025-09-14 16:37:01 +0100

    john

--- index.py
+++ index.py
@@ -19,3 +19,27 @@ if a > 0 or b > 0:
     print("either number is greater than 0")
 else: 
     print(" no number is greater than 0")
+
+
+
+#not equal operator
+a = 10 
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+a = "python"
+b = "coding"
+if a != b:
+    print(a,'and',b, "are different") 
+
+    a=5
+    b=5
+
+if(a == 1) != (b==5):
+    print( "hello")
+
+
+a=int (input("enter a number"))
+if a%2 !=0 : 
+    print( a,"is not an even number")
