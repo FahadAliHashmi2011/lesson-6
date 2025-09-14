25edaf49023c4254462623454cd2027211b5289a
FahadAliHashmi2011 <faiezali2006@gmail>
2025-09-14 16:51:29 +0100

    billy bob jr

--- index.py
+++ index.py
@@ -43,3 +43,57 @@ if(a == 1) != (b==5):
 a=int (input("enter a number"))
 if a%2 !=0 : 
     print( a,"is not an even number")
+
+
+# bmi checker  
+height = float(input("please enter your height in cm"))
+weight = float(input("please enter your weight in kg"))
+
+bmi = weight / (height/100)**2
+print("ur BMI is",bmi)
+
+if bmi<=18.4:
+    print("u are underweight")
+elif bmi<=24.9:
+    print("u are healthy")
+elif bmi<=29.9:
+    print("u are overweight")
+
+elif bmi<=34.9:
+    print("u are severley overweight")
+elif bmi<=39.9:
+    print("u are obese")
+else:
+    print(" u are sevrely obese")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
